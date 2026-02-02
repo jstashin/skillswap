5c22bb7d437a79a5575c8e23a8d97a3a878d300b
Jerin Sayma <jstashin2021@gmail.com>
2026-02-02 15:29:34 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAehaEQdmcxnDIfI_n5qyhUUXUmXYjTzKg",
-  authDomain: "skillswap-skill.firebaseapp.com",
-  projectId: "skillswap-skill",
-  storageBucket: "skillswap-skill.firebasestorage.app",
-  messagingSenderId: "525016836399",
-  appId: "1:525016836399:web:7a08dfc0215e14295459da",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
